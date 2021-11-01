35acefa3c55da6c6f2eef2bcca3ba95b29dfd645
Ozioma <oziomaokoroafor@gmail.com>
2021-11-01 15:17:51 +0100

    Loops in Javascript commit- added asterisks and stars printing program

--- selfwork/chapter3ProgramStructure/loopsInJavaScript.js
+++ selfwork/chapter3ProgramStructure/loopsInJavaScript.js
@@ -1,24 +1,26 @@
+// import * as readline from 'node:readline/promises';
+
 for(let counter = 1; counter < 8; counter++){
     let asterisks = "#".repeat(counter)
     console.log(asterisks)
 }
 
 for (let counter = 1; counter<=100; counter++){
-    if (counter%3 ==0 && counter%5 ==0){
+    if (counter%3 ===0 && counter%5 ===0){
         console.log("FizzBuzz")
     }
-    if (counter %3 ==0){
+    if (counter %3 ===0){
         console.log("Fizz")
-    }else if (counter%5 ==0){
+    }else if (counter%5 ===0){
         console.log("Buzz")
     }else console.log(counter)
 }
 
-//
-// let firstString = " # # # #"
-// let secondString= "# # # # "
-// let value = prompt("Enter the length of the chessboard")
-// for (let counter = 0; counter<value ; counter++){
-//     if (counter%2 ===0)console.log(firstString)
-//     else if (counter%2 ===1)console.log(secondString)
-// }
+
+let firstString = " # # # #"
+let secondString= "# # # # "
+let value = 8
+for (let counter = 0; counter<value ; counter++){
+    if (counter%2 ===0)console.log(firstString)
+    else if (counter%2 ===1)console.log(secondString)
+}
