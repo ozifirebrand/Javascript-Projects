849a2c6520392f6f9c38486d450ca64cda8132ef
Ozioma <oziomaokoroafor@gmail.com>
2021-11-01 21:16:02 +0100

    Find the minimum of two numbers Function

--- selfwork/chapter3Functions/closureInFunctions.js
+++ selfwork/chapter3Functions/closureInFunctions.js
@@ -5,3 +5,21 @@ function multiplier(factor){
 twice = multiplier(2)
 console.log(twice(5))
 
+let findTheMinimumValue = (firstValue, secondValue) => {
+    if (firstValue < secondValue){
+        console.log(firstValue +" is the minimum value")
+    }
+    else console.log(secondValue + " is the minimum value")
+}
+
+findTheMinimumValue(4, 7)
+//
+// let isEven = (aNumber)=>{
+//     let reduceTo1 = (value)=>value-2;
+//     if (aNumber===0) return true;
+//     else if (aNumber===1) return false
+//     else if (aNumber -2 > 1) reduceTo1(aNumber)
+//     return isEven(aNumber)
+// }
+//
+// console.log(isEven(15))
