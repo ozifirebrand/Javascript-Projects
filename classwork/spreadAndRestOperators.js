bf90b40e9d3b7979250a8916a56aee1eb2e3e682
Ozioma <oziomaokoroafor@gmail.com>
2021-11-05 11:25:19 +0100

    Spread and other functions

---
+++ classwork/spreadAndRestOperators.js
@@ -0,0 +1,10 @@
+let persons = {
+    person1: "jerry",
+    person2: "saheed",
+    person3: "demola",
+    person4: "dami",
+}
+
+console.log("1", persons)
+let sheyi  = {...persons}
+console.log("2", sheyi)
