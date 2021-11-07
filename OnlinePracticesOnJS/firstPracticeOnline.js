9e3c680c4972fa2b0b740281243f27c015334445
Ozioma <oziomaokoroafor@gmail.com>
2021-11-07 20:31:20 +0100

    Reversing a string

--- OnlinePracticesOnJS/firstPracticeOnline.js
+++ OnlinePracticesOnJS/firstPracticeOnline.js
@@ -11,3 +11,30 @@ let greeting = "Hello"
  console.log(strr);
  let back = Boolean(strr)
  console.log(back)
+
+ let value = greeting.lastIndexOf("o")
+ let substr = greeting.slice(1, value)
+ console.log(substr)
+
+ let newArray = [5, 3, 32, 6, 9]
+ let lastNumber = newArray.pop()
+ console.log(lastNumber)
+ let firstNumber = newArray.shift();
+ console.log(firstNumber)
+
+ newArray.push(8, 3, 22)
+ console.log(newArray)
+
+ function reverse(word){
+     let reversedString ='';
+     let arr = [];
+     for (let i = 0; i < word.length ; i++) {
+         arr.push(word[i])
+     }
+     while (arr.length>0){
+         reversedString += arr.pop()
+     }
+     return reversedString
+ }
+
+ console.log(reverse("seyiObembe"))
