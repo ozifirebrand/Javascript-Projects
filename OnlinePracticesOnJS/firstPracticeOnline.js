c03088834c25fd84cff83a281571482119c9c834
Ozioma <oziomaokoroafor@gmail.com>
2021-11-07 20:07:24 +0100

    First practice in JS online

---
+++ OnlinePracticesOnJS/firstPracticeOnline.js
@@ -0,0 +1,13 @@
+ let str = "Hi";
+let greeting = "Hello"
+
+ let name = "John"
+ let message = `${str} ${name} \n${greeting} ${name}`
+
+ console.log(message)
+
+ let status = false;
+ let strr = status.toString();
+ console.log(strr);
+ let back = Boolean(strr)
+ console.log(back)
