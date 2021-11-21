8bb2175e37dd53a716acb55d2d0a4bf8804624ba
Ozioma <oziomaokoroafor@gmail.com>
2021-11-21 15:28:37 +0100

    Reversing an array program

--- selfwork/chapter4DataStructuresObjectsAndArrays/reversingAnArray.js
+++ selfwork/chapter4DataStructuresObjectsAndArrays/reversingAnArray.js
@@ -11,15 +11,11 @@ reverseArray(array)
 
 function reverseArrayInPlace(anArray){
     for (let index1 = 0 ; index1<(anArray.length)/2; index1++){
-        console.log(anArray[index1])
-        for (let index2 = anArray.length-1; index2> (anArray.length)/2; index2--){
-            console.log(anArray[index2])
-            //todo 4329
-            let aValue = anArray[index1]
-            anArray[index1] = anArray[index2]
-            anArray[index2] = aValue
-            console.log(anArray[index2])
-
+        for (let index2 = anArray.length-1; index2>= (anArray.length)/2; index2--){
+            let aValue = anArray[index2]
+            anArray[index2] = anArray[index1]
+            console.log(aValue, index2)
+            // anArray[index1] = aValue
         }
     }
     return anArray
