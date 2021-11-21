d98f0b78a6510d6d4b696c6ede2c6e639c5cdc21
Ozioma <oziomaokoroafor@gmail.com>
2021-11-21 02:34:29 +0100

    Sum of a range program refactored

--- selfwork/chapter4DataStructuresObjectsAndArrays/sumOfARange.js
+++ selfwork/chapter4DataStructuresObjectsAndArrays/sumOfARange.js
@@ -1,15 +1,33 @@
 function range(start, end, step){
     let array = []
-    while (start<=end){
-        array.push(start)
-        if (step === undefined || step ===null){
-            start++
-        }else {
-            start +=step
+    if (start <end) {
+        while (start <= end) {
+            array.push(start)
+            if (step === undefined || step === null) {
+                start++
+            } else {
+                start += step
+            }
+        }
+    }else if (start>end){
+        while (start>=end){
+            array.push(start)
+            if (step === undefined || step === null) {
+                start--
+            } else {
+                start += step
+            }
         }
     }
     return array
 }
+//
+// function increaseStart(start, step){
+//     if (step === undefined || step === null || step===1) {
+//         return start+=1
+//     }
+//     return start +=step
+// }
 
 function sumOfRange(array){
     let sum=0;
@@ -20,6 +38,6 @@ function sumOfRange(array){
 }
 
 console.log(range(1, 10))
-console.log(range(1, 10, 2))
-let array = range(1, 10, )
+console.log(range(10, 1, -2))
+let array = range(1, 10)
 console.log(sumOfRange(array))
